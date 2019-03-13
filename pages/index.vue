844ba9f446c4aa33749c32d64663c290d860e709
root <root@DESKTOP-BS91BGQ.localdomain>
2019-03-13 17:47:30 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="container">
+    <div class="alert alert-success">xx</div>
+  </section>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+
+</style>
