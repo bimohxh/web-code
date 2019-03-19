c751adb7635f9630f4d82fec38732607db409893
root <root@DESKTOP-BS91BGQ.localdomain>
2019-03-19 18:40:20 +0800

    more

--- web/pages/code/_id/index.vue
+++ web/pages/code/_id/index.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-3">
+      <div :class="showList ? 'col-3' : 'col-0'">
         <div class="card code-list">
-          <ul class="list-group list-group-flush">
+          <ul class="list-group list-group-flush"  v-show="showList">
+            <div class="list-group-item search-box">
+              <input type="text" placeholder="关键字回车搜索..." style="border: none"/>
+              <icon name="search" />
+            </div>
             <router-link :to="'/code/' + code.id" v-for="code in codes" :key="code.title" class="list-group-item">
               {{ code.title }}
             </router-link>
           </ul>
+          <a class="toggle-btn" href="javascript: void(0)" @click="showList = false">
+            <icon name="chevron-left" />
+          </a>
         </div>
       </div>
-      <div class="col-9">
+      <div :class="showList ? 'col-9' : 'col-12'">
         <section class="code-box">
           <div class="card mb-2 card-code" v-for="(file, index) in code.files" :key="file.id" >
             <div class="card-header d-flex">
@@ -84,6 +91,7 @@ export default {
   },
   data() {
     return {
+      showList: true,
       codeEditorOptions: {
         lineNumbers: true,
         theme: 'paraiso-dark'
@@ -114,6 +122,7 @@ export default {
 <style lang="scss">
 .code-box {
   max-width: 800px;
+  margin: 0 auto;
 }
 
 .card-code {
@@ -177,12 +186,54 @@ export default {
 }
 
 .code-list {
+  overflow-y: auto;
+  position: fixed;
+  min-width: 250px;
+  top: 80px;
+  bottom: 10px;
   a.list-group-item {
     text-decoration: none;
     color: #34495e;
     &:hover {
       background-color: #f7f8fa;
     }
+  }
+  .search-box {
+    padding: 0;
+    position: relative;
+    padding-right: 30px;
+    input {
+      border: none;
+      padding: 12px 10px;
+      width: 100%;
+      outline: none;
+      background: none;
+      &::placeholder {
+        color: #CCC
+      }
+    }
+    .icon {
+      position: absolute;
+      right: 6px;
+      top: 10px;
+      color: #DDD;
+    }
+  }
+  .toggle-btn {
+    width: 30px;
+    height: 60px;
+    position: absolute;
+    right: 0;
+    top: 50%;
+    margin-top: -50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FFF;
+    box-shadow: 0px 0px 5px #DDD;
+    border-top-left-radius: 3px;
+    border-bottom-left-radius: 3px;
+    opacity: 0.6
   }
 }
 </style>
