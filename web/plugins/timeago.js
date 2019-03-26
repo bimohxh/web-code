41700c180014ed2c94e9bc91c8b7849a77d5a821
root <root@DESKTOP-BS91BGQ.localdomain>
2019-03-26 16:49:17 +0800

    more

--- web/plugins/timeago.js
+++ web/plugins/timeago.js
@@ -2,6 +2,6 @@ import { format } from 'timeago.js'
 
 export default ({ app, req }, inject) => {
   inject('timeago', (date) => {
-    reutrn format(date, 'zh_CN')
+    return format(date, 'zh_CN')
   })
 }
